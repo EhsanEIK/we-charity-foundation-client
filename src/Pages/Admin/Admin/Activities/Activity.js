d3500b6dd4459d7295085e7bdd97e8a259a64dce
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-07 13:44:32 +0600

    added handleAddMyActivity function in Activity.js file

--- src/Pages/Admin/Admin/Activities/Activity.js
+++ src/Pages/Admin/Admin/Activities/Activity.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
+import { AuthContext } from '../../../../contexts/AuthProvider/AuthProvider';
 
 const Activity = ({ activity }) => {
+    const { user } = useContext(AuthContext);
     const { _id, title, banner, date } = activity;
+
+    const handleAddMyActivity = () => {
+        const myActivity = {
+            activityId: _id,
+            email: user?.email,
+            title,
+            banner,
+            date,
+        }
+        fetch('http://localhost:5000/myActivities', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(myActivity),
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    toast.success('Added the activity in your profile');
+                }
+            })
+    }
 
     return (
         <div className="max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
@@ -16,7 +42,7 @@ const Activity = ({ activity }) => {
 
             <div className="flex items-center justify-between px-4 py-2 bg-gray-900">
                 <h1 className="text-lg font-bold text-white">{date}</h1>
-                <button className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">Going</button>
+                <button onClick={handleAddMyActivity} className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">Going</button>
             </div>
         </div>
     );
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -63,13 +63,15 @@ const Header = () => {
                     </div>
 
                     <div className="flex justify-center md:block">
-                        <a className="relative text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300" href="#">
-                            <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
+                        <div className="flex">
+                            <button type="button" class="flex items-center focus:outline-none" aria-label="toggle profile dropdown">
+                                <div class="w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full">
+                                    <img src={user?.photoURL} alt="avatar" />
+                                </div>
 
-                            <span className="absolute top-0 left-0 p-1 text-xs text-white bg-blue-500 rounded-full"></span>
-                        </a>
+                                <h3 class="mx-2 text-gray-700 dark:text-gray-200 lg:hidden">Khatab wedaa</h3>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div >
