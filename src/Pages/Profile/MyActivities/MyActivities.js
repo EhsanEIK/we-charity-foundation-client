e46b437d13eecdfcc26de00686d527cabf0d3bf7
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-07 14:50:39 +0600

    added handleDeleteMyActivity function to cancel/delete activity from user profile

--- src/Pages/Profile/MyActivities/MyActivities.js
+++ src/Pages/Profile/MyActivities/MyActivities.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 import MyActivity from './MyActivity';
 
@@ -12,13 +13,31 @@ const MyActivities = () => {
             .then(data => setMyActivities(data));
     }, [user?.email]);
 
+    const handleDeleteMyActivity = (id, title) => {
+        const agree = window.confirm("Are you sure to cancel this?");
+        if (agree) {
+            fetch(`http://localhost:5000/myActivities/${id}`, {
+                method: "DELETE",
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        toast.success(`Cancel ${title} event successfully and removed from your activities`);
+                        const remainingMyActivities = myActivities.filter(activity => activity._id !== id);
+                        setMyActivities(remainingMyActivities);
+                    }
+                })
+        }
+    }
+
     return (
         <div>
             <div className='grid grid-cols-2 gap-5'>
                 {
                     myActivities.map(myActivity => <MyActivity
                         key={myActivity._id}
-                        myActivity={myActivity}></MyActivity>)
+                        myActivity={myActivity}
+                        handleDeleteMyActivity={handleDeleteMyActivity}></MyActivity>)
                 }
             </div>
         </div>
--- src/Pages/Profile/MyActivities/MyActivity.js
+++ src/Pages/Profile/MyActivities/MyActivity.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const MyActivity = ({ myActivity }) => {
+const MyActivity = ({ myActivity, handleDeleteMyActivity }) => {
     const { _id, activityId, title, date, banner } = myActivity;
 
     const [acitivity, setMyAcitivity] = useState([]);
@@ -25,7 +25,7 @@ const MyActivity = ({ myActivity }) => {
 
                 <div className="flex justify-between mt-3 item-center">
                     <h1 className="text-sm font-bold text-gray-700 dark:text-gray-200">{date}</h1>
-                    <button className="px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-300 transform bg-red-700 rounded dark:bg-gray-700 hover:bg-red-600 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">cancel</button>
+                    <button onClick={() => handleDeleteMyActivity(_id, title)} className="px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-300 transform bg-red-700 rounded dark:bg-gray-700 hover:bg-red-600 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">cancel</button>
                 </div>
             </div>
         </div >
